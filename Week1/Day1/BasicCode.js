1408b48e8141a3727a59e902cf920757c051bae5
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-01-20 21:24:47 +0530

    w1.day1

---
+++ Week1/Day1/BasicCode.js
@@ -0,0 +1,4 @@
+console.log("Welcome To TestLeaf")
+console.log("Playwright with TypeScript")
+
+//node FileName.js
---
+++ Week1/Day1/DataType.js
@@ -0,0 +1,14 @@
+var idNo=3458
+var comName=`TestLeaf`
+var status=true
+var aadharNo=7234567890785623456n
+var empName
+var emp=null
+// file name -> filename.js
+//execute -> node filename.js
+
+console.log("EMP ID No :"+idNo)
+console.log(`Company name is `,comName)
+console.log('My aadhar Number '+aadharNo +" "+typeof(aadharNo))
+console.log("Undefined ",empName)
+console.log("Null "+emp)
